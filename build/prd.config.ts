003fa963a69d47b8dce77ab187367e84a4bf1b31
Jinbo Wang <che3vinci@gmail.com>
2021-11-08 00:04:32 +0800

    1.0.5

--- build/prd.config.ts
+++ build/prd.config.ts
@@ -3,7 +3,7 @@ import baseConfig from './base.config';
 import webpack, { Configuration } from 'webpack';
 
 export default merge(baseConfig, {
-  mode: 'development',
+  mode: 'production',
   plugins: [
     new webpack.DefinePlugin({
       __DEV__: 'false',
